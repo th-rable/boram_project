1fdb707c7a9cfebace7931b1437ba02b790f3f99
Three_Apple <73579842+ThreeApple0@users.noreply.github.com>
2022-07-08 15:53:32 +0900

    20220708

--- app/src/main/java/com/example/boram_project/MainActivity.kt
+++ app/src/main/java/com/example/boram_project/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.boram_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.format.Time
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import java.time.LocalDateTime
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var database: DatabaseReference
+// ...
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun firebasetestclick(){
+        database = Firebase.database.reference
+
+
+        database.child("test").setValue(LocalDateTime.now());
     }
 }
